70d75998569843db3d59c280b411daedc91433e1
Feven <feventalaku48@gmail.com>
2021-06-20 21:07:51 -0600

    <first commit>

---
+++ Learning/src/main/java/com/mes/learning/LearningOne.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mes.learning;
+
+/**
+ *
+ * @author Owner
+ */
+public class LearningOne {
+    public static void main(String[] args) {
+        System.out.println("The quick fox jump over the lazy dog");
+    }
+    
+}
