29f9c9a125cb33bc90bb8346295a8465d783b193
feventalaku <86210927+feventalaku@users.noreply.github.com>
2021-06-21 19:45:13 -0600

    <resume added>

---
+++ Project1/src/main/java/practice/ProjectNumberOne.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice;
+
+/**
+ *
+ * @author Owner
+ */
+public class ProjectNumberOne {
+    public static void main(String[] args) {
+        //Resume project
+        System.out.println("·	To find a challenging career in a respected");
+        System.out.println(" organization, so that I may get new experiences, ");
+        System.out.println("grow professionally, and be part of a team that");
+        System.out.println("makes a difference in a society");
+        
+        
+    }
+    
+}
